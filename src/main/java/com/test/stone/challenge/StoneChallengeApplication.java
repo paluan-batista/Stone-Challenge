d8cb3f3362cc6ab320a3e3bdd8b86e50fae9db7e
Paluan Batista <48101122+paluan-batista@users.noreply.github.com>
2021-02-04 15:43:05 -0300

    Add project on repository

---
+++ src/main/java/com/test/stone/challenge/StoneChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.test.stone.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/stone/challenge/StoneChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.stone.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoneChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
