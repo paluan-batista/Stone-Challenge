1c16ae7fda4e3670a2cc72cc9614e84dcff7c1a1
Paluan Batista <paluan.desenvolvimento@gmail.com>
2021-02-05 02:35:08 -0300

    Add interface implementations

--- src/main/java/com/stone/challenge/impl/ShoppingListImpl.java
+++ src/main/java/com/stone/challenge/impl/ShoppingListImpl.java
@@ -1,5 +1,41 @@
 package com.stone.challenge.impl;
 
-public class ShoppingListImpl {
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.stone.challenge.model.Person;
+import com.stone.challenge.service.ShoppingListService;
+
+@Component
+public class ShoppingListImpl implements ShoppingListInterface {
+
+	@Autowired
+	private ShoppingListService service;
+
+	@Override
+	public List<Person> findAll() throws IOException {
+
+		return service.findAll();
+	}
+
+	@Override
+	public Person findById(Integer id) throws IOException {
+		return service.findById(id);
+	}
+
+	@Override
+	public Person findByEmail(String email) throws IOException {
+		return service.findByEmail(email);
+	}
+
+	@Override
+	public Map<String, String> getBalancePayablePerPerson(String email) throws IOException {
+		
+		return service.getBalancePayablePerPerson(email);
+	}
 
 }
--- src/main/java/com/stone/challenge/impl/ShoppingListInterface.java
+++ src/main/java/com/stone/challenge/impl/ShoppingListInterface.java
@@ -1,5 +1,19 @@
 package com.stone.challenge.impl;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.stone.challenge.model.Person;
+
 public interface ShoppingListInterface {
+	
+	public List<Person> findAll() throws IOException;
+	
+	public Person findById(Integer id) throws IOException;
+	
+	public Person findByEmail(String email) throws IOException;
+	
+	public Map<String, String> getBalancePayablePerPerson(String email) throws IOException;
 
 }
