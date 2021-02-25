c08d10a3196fd1fac273d1013663f9663c3d3f61
Paluan Batista <48101122+paluan-batista@users.noreply.github.com>
2021-02-25 00:25:08 -0300

    update name method

--- src/main/java/com/stone/challenge/config/SwaggerConfig.java
+++ src/main/java/com/stone/challenge/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 	@Bean
-	public Docket cidadesApi() {
+	public Docket shoppingListApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build().apiInfo(metaInfo());
 	}
